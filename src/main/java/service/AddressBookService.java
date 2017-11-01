e362856b1a7cff74e48679b19e4c8133ea059694
Alex Mitchem <amcodeprojects@gmail.com>
2017-11-02 09:26:06 +1100

    added unique constraint on Contact.name and added searchByName and removeByName functionality to AddressBookService

--- src/main/java/service/AddressBookService.java
+++ src/main/java/service/AddressBookService.java
@@ -3,16 +3,33 @@ package service;
 import model.Contact;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class AddressBookService {
-    List<Contact> contactList = new ArrayList<>();
+    Map<String, Contact> contactList = new HashMap<>();
 
-    public void addNewEntry(String testName, String testPoneNumber) {
-        contactList.add(new Contact(testName, testPoneNumber));
+    public boolean addNewEntry(String contactName, String phoneNumber) {
+        if (contactList.containsKey(contactName)) {
+            return false;
+        } else{
+            contactList.put(contactName, new Contact(contactName, phoneNumber));
+            return true;
+        }
     }
 
     public List<Contact> getAllEntries() {
-        return this.contactList;
+        List<Contact> entryList = new ArrayList<>();
+        entryList.addAll(contactList.values());
+        return entryList;
+    }
+
+    public Contact searchByName(String contactName) {
+        return contactList.get(contactName);
+    }
+
+    public void removeByName(String contactName) {
+        contactList.remove(contactName);
     }
 }
--- src/test/java/service/AddressBookServiceTest.java
+++ src/test/java/service/AddressBookServiceTest.java
@@ -8,25 +8,54 @@ import org.mockito.Mock;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class AddressBookServiceTest {
     private AddressBookService addressBookService;
     private String testName = "test name";
     private String testPhoneNumber = "+00 1234567890";
+
     @Before
     public void setUp() throws Exception {
         addressBookService = new AddressBookService();
     }
 
     @Test
-    public void shouldReturnAnEmptyListWhenThereAreNoAddresses(){
+    public void getAllEntriesReturnsAnEmptyListWhenThereAreNoAddresses() {
         assertEquals(Collections.emptyList(), addressBookService.getAllEntries());
     }
 
     @Test
-    public void shouldAddNewEntriesToTheList(){
+    public void addNewEntryAddsANewEntryToTheList() {
         addressBookService.addNewEntry(testName, testPhoneNumber);
         assertEquals(1, addressBookService.getAllEntries().size());
+    }
+
+    @Test
+    public void searchByNameReturnsNullWhenNoEntriesMatchTheNameGiven() {
+        addressBookService.addNewEntry(testName, testPhoneNumber);
+        assertNull(addressBookService.searchByName("false name"));
+    }
+
+    @Test
+    public void searchByNameReturnsContactMatchingTheNameGiven() {
+        assert (addressBookService.addNewEntry(testName, testPhoneNumber));
+        assertEquals(new Contact(testName, testPhoneNumber), addressBookService.searchByName(testName));
+    }
+
+    @Test
+    public void addNewEntryDoesNotAddANewEntryIfThereExistsAnEntryWithTheSameName() {
+        addressBookService.addNewEntry(testName, testPhoneNumber);
+        String duplicatePhoneNumber = "0000 0000";
+        assertFalse(addressBookService.addNewEntry(testName, duplicatePhoneNumber));
+        assertNotEquals(new Contact(testName, duplicatePhoneNumber), addressBookService.searchByName(testName));
+    }
+
+    @Test
+    public void removeByNameRemovesTheEntryMatchingTheNameGiven() {
+        addressBookService.addNewEntry(testName, testPhoneNumber);
+        addressBookService.removeByName(testName);
+        assertNull(addressBookService.searchByName(testName));
     }
 }
